4bee6f21a5c58e71d8577572235265dc430b260f
svetlichnaya.org@gmail.com <svetlichnaya.org@gmail.com>
2023-06-26 00:18:09 +0200

    changing

---
+++ src/app/client-info.spec.ts
@@ -0,0 +1,7 @@
+import { ClientInfo } from './client-info';
+
+describe('ClientInfo', () => {
+  it('should create an instance', () => {
+    expect(new ClientInfo('','','','','','','','','','')).toBeTruthy();
+  });
+});
